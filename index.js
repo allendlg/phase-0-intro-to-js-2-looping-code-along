f7afca8541df5e639bb780bbb5d68ff781cfc97f
Allen de la Garza <allen.delagarza01@gmail.com>
2021-06-20 21:34:53 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+//const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+//const event = 'surprise';
+let newArr = [];
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;}
+  
+
+    return newArr;
+  
+}
+function countDown(num) {
+    let loop = num
+    while (loop >= 0) {
+        console.log(loop--);
+    }
+}
